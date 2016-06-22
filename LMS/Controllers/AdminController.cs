cae8c6a6751b71ecf260d250760e89cbbf1438f8
baobaoxinliku <crazyxuc@qq.com>
2016-06-22 09:53:15 +0800

    06220952

--- LMS/Controllers/AdminController.cs
+++ LMS/Controllers/AdminController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace LMS.Controllers
 {
@@ -31,6 +32,29 @@ namespace LMS.Controllers
             db.Admins.Add(admin);
             db.SaveChanges();
             return RedirectToAction("Index");
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Models.Admin admin)
+        {
+            var user = db.Admins.Where(b => b.AdminAccount == admin.AdminAccount & b.AdminPwd == admin.AdminPwd);
+            if (user.Count() > 0)
+            {
+                ViewBag.LoginState = "登录成功";
+                FormsAuthentication.SetAuthCookie(admin.AdminAccount, false);
+                //Session["UserID"] = user.First().UserName;
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.LoginState = "登录失败";
+                //ModelState.AddModelError("CredentitalError", "Invalid Username or Password");
+                return View();
+            }
         }
 
     }
---
+++ LMS/ViewModels/AccountListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LMS.ViewModels
+{
+    public class AccountListViewModel
+    {
+        public List<Models.Admin> Admin { get; set; }
+    }
+}
